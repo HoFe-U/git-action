80287ec29bf652f33d7171de9a812ec8a8ed28d3
HoFe-U <hochul-y@naver.com>
2022-12-20 16:47:16 +0900

    [FEAT] test

--- src/main/java/com/example/gitaction/GitActionApplication.java
+++ src/main/java/com/example/gitaction/GitActionApplication.java
@@ -1,5 +1,7 @@
 package com.example.gitaction;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class GitActionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitActionApplication.class, args);
+        System.out.println("test");
     }
 
 }
